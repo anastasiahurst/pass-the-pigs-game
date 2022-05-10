d4dc778f905c1ff6007c1f5a29c4322185c58440
Anastasia Hurst <anastasia.kohanuk@gmail.com>
2022-05-10 21:10:20 +0100

    switching player functionality complete

--- pigs_js.js
+++ pigs_js.js
@@ -1,28 +1,27 @@
+"use strict";
+
 // selecting elements
+let player0El = document.querySelector(".player0_container");
+let player1El = document.querySelector(".player1_container");
 let score0El = document.querySelector(".total_score_player0");
 let score1El = document.querySelector(".total_score_player1");
 let current0El = document.querySelector(".current_score_player0");
 let current1El = document.querySelector(".current_score_player1");
-let currentScoreEl = document.querySelector(".current_score");
+
 let diceEl = document.querySelector(".dice");
+
 let btnRoll = document.querySelector(".btn--roll");
 let btnHold = document.querySelector(".btn--hold");
 let btnNew = document.querySelector(".btn--new");
 
-let score0ElValue = score0El.textContent;
-let score1ElValue = score1El.textContent;
-let current0ElValue = current0El.textContent;
-let current1ElValue = current1El.textContent;
-let currentScoreValue = currentScoreEl.textContent;
-
-score0ElValue = 0;
-score1ElValue = 0;
-current0ElValue = 0;
-current1ElValue = 0;
-currentScoreValue = 0;
+let currentScore = 0;
+let activePlayer = 0;
 
 // adding HIDDEN class to the dice, so it disappears
 diceEl.classList.add("hidden");
+document
+  .getElementById(`player${activePlayer}_container`)
+  .classList.add("active_overlay");
 
 // rolling dice functionality
 btnRoll.addEventListener("click", function () {
@@ -31,12 +30,20 @@ btnRoll.addEventListener("click", function () {
   diceEl.src = `dice${diceNumber}.png`;
 
   if (diceNumber !== 1) {
-    currentScoreValue += diceNumber;
-    console.log(currentScoreValue);
-    current0El.textContent = currentScoreValue;
+    currentScore += diceNumber;
+    console.log(currentScore);
+    document.getElementById(`current_score_player${activePlayer}`).textContent =
+      currentScore; /* CHANGE LATER */
   } else {
+    // switch to next player
+    document.getElementById(
+      `current_score_player${activePlayer}`
+    ).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle("active_overlay");
+    player1El.classList.toggle("active_overlay");
   }
 });
 
-// the above console.log(currentScore) output equalt to [object HTMLParagraphElement] + the number. for example, if the output should have been number 5, then it writes [object HTMLParagraphElement]5?
-// The working code should display the correct dice score to the Current Score of player 1.
+// holding score functionality
